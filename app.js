54fe6a27ce0da7f6cb7b80a8e86f0ac9793535ed
Anjan Poonacha <anjanpoonacha369@gmail.com>
2019-11-27 17:58:14 +0530

    initial commit

--- app.js
+++ app.js
@@ -14,13 +14,16 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log(`Hello from the middleware`);
-
   req.requestTime = new Date().toUTCString();
   next();
 });
 
 app.use(`/api/v1/tours`, tourRouter);
 app.use(`/api/v1/users`, userRouter);
-
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'FAIL',
+    message: `Can't find ${req.originalUrl} on this server`
+  });
+});
 module.exports = app;
